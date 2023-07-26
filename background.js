d57e605ed30fac8506373a09d137a26c2743626e
Adam Wadsworth <32913685+highfly117@users.noreply.github.com>
2023-07-26 14:22:08 +0100

    Change options page to a popup and bug fixes

--- background.js
+++ background.js
@@ -1,20 +1,17 @@
 // background.js
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-
     if (tab.url.includes("https://www.lttstore.com/cart")) {
         return;
-      }
-
+    }
     if (changeInfo.status === 'complete' && tab.url.startsWith('http')) {
-        chrome.storage.sync.get(['taxRate', 'currency', 'rates', 'lastFetch'], function (data) {
+        chrome.storage.sync.get(['taxRate', 'currency', 'rates', 'lastFetch', 'shippingCost'], function (data) {
             let taxRate = data.taxRate || 0;
             let currency = data.currency || 'USD';
             let rates = data.rates || {};
             let lastFetch = data.lastFetch;
-
+            let shippingCost = Number(data.shippingCost) || 19.99;
             let now = Date.now();
             let twentyFourHours = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-
             if (!lastFetch || now - lastFetch > twentyFourHours) {
                 // Rates are more than 24 hours old or don't exist, fetch them
                 fetch('https://open.er-api.com/v6/latest/USD')
@@ -22,75 +19,106 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                     .then(data => {
                         rates = data.rates; // Extract the rates from the response
                         chrome.storage.sync.set({ rates: rates, lastFetch: now }, function () {
-                            executeScript(tabId, taxRate, currency, rates);
+                            executeScript(tabId, taxRate, currency, rates, shippingCost);
                         });
                     });
             } else {
                 // Rates are less than 24 hours old, use them
-                executeScript(tabId, taxRate, currency, rates);
+                executeScript(tabId, taxRate, currency, rates, shippingCost);
             }
         });
     }
 });
 
-function executeScript(tabId, taxRate, currency, rates) {
+function executeScript(tabId, taxRate, currency, rates, shippingCost) {
     chrome.scripting.executeScript({
         target: { tabId: tabId },
-        function: replacePrices,
-        args: [taxRate, currency, rates]
-    });
-}
+        function: function (taxRate, currency, rates, shippingCost) {
+            function currencySymbol(currency) {
+                switch (currency) {
+                    case 'USD':
+                        return '$';
+                    case 'EUR':
+                        return '€';
+                    case 'GBP':
+                        return '£';
+                    default:
+                        return currency + " ";
+                }
+            }
 
-function replacePrices(taxRate, currency, rates) {
-    function currencySymbol(currency) {
-        switch (currency) {
-            case 'USD':
-                return '$';
-            case 'EUR':
-                return '€';
-            case 'GBP':
-                return '£';
-            // Add more cases here for other currencies
-            default:
-                return '';
-        }
-    }
+            function convertCurrency(amount, currency, rates) {
+                let rate = rates[currency];
+                return amount * rate;
+            }
 
-    function convertCurrency(amount, currency, rates) {
-        // The exchange rate to USD
-        let rate = rates[currency];
-        return amount * rate;
-    }
+            function replacePrices() {
+                let priceSpans = document.querySelectorAll('.money');
+                priceSpans.forEach(priceSpan => {
+                    let appendedSpans = priceSpan.parentNode.querySelectorAll('.appended');
+                    appendedSpans.forEach(span => span.remove());
+                    if (priceSpan) {
+                        let originalPrice = parseFloat(priceSpan.getAttribute('data-original-price'));
+                        if (isNaN(originalPrice)) {
+                            originalPrice = parseFloat(priceSpan.textContent.replace(/[^\d.-]/g, ''));
+                            priceSpan.setAttribute('data-original-price', originalPrice);
+                        }
+                        let convertedPrice = convertCurrency(originalPrice, currency, rates);
+                        let shippingCostConverted = convertCurrency(shippingCost, currency, rates);
+                        let costBeforeTax = convertedPrice + shippingCostConverted;
+                        let taxAmount = costBeforeTax * taxRate / 100;
+                        let totalCost = costBeforeTax + taxAmount;
+                        priceSpan.textContent = `${currencySymbol(currency)}${convertedPrice.toFixed(2)}`;
+                        let shippingSpan = document.createElement("span");
+                        shippingSpan.className = 'appended';
+                        shippingSpan.textContent = ` (+ Shipping ${currencySymbol(currency)}${shippingCostConverted.toFixed(2)})`;
+                        priceSpan.parentNode.appendChild(shippingSpan);
+                        let taxSpan = document.createElement("span");
+                        taxSpan.className = 'appended';
+                        taxSpan.textContent = ` (Tax: ${currencySymbol(currency)}${taxAmount.toFixed(2)})`;
+                        priceSpan.parentNode.appendChild(taxSpan);
+                        let totalSpan = document.createElement("span");
+                        totalSpan.className = 'appended';
+                        totalSpan.textContent = ` (Total: ${currencySymbol(currency)}${totalCost.toFixed(2)})`;
+                        priceSpan.parentNode.appendChild(totalSpan);
+                    }
+                });
+                watchPriceElement(`#price-price-template--14646036037735__main`, taxRate, currency, rates);
+                setupSizeChangeListeners(taxRate, currency, rates);
+            }
+            
+            
 
-    let priceSpans = document.querySelectorAll('.money');
-  priceSpans.forEach(priceSpan => {
-    if (priceSpan) {
-        let price = parseFloat(priceSpan.textContent.replace(/[^\d.-]/g, ''));
-        let convertedPrice = convertCurrency(price, currency, rates);
-  
-        // add shipping
-        let shippingCost = convertCurrency(19.99, currency, rates);
-        let costBeforeTax = convertedPrice + shippingCost;
-        
-        // calculate tax
-        let taxAmount = costBeforeTax * taxRate / 100;
-        
-        // display the converted price
-        priceSpan.textContent = `${currencySymbol(currency)}${convertedPrice.toFixed(2)}`;
-  
-        let shippingSpan = document.createElement("span");
-        shippingSpan.textContent = ` (+ Shipping ${currencySymbol(currency)}${shippingCost.toFixed(2)})`;
-        priceSpan.parentNode.appendChild(shippingSpan);
-  
-        let taxSpan = document.createElement("span");
-        taxSpan.textContent = ` (Tax: ${currencySymbol(currency)}${taxAmount.toFixed(2)})`;
-        priceSpan.parentNode.appendChild(taxSpan);
-  
-        // Calculate total cost (price + tax) and add it
-        let totalCost = costBeforeTax + taxAmount;
-        let totalSpan = document.createElement("span");
-        totalSpan.textContent = ` (Total: ${currencySymbol(currency)}${totalCost.toFixed(2)})`;
-        priceSpan.parentNode.appendChild(totalSpan);
-        }
+            function setupSizeChangeListeners(taxRate, currency, rates) {
+                // Get all size selection radio buttons
+                let sizeRadioButtons = document.querySelectorAll('.product-variant-size, .ColorSwatch__Radio');
+
+                // Add an event listener to each radio button
+                sizeRadioButtons.forEach(button => {
+                    button.addEventListener('change', () => {
+                        // When a size option is selected, wait a short moment to allow the site to update the price,
+                        // then replace the prices
+                        setTimeout(() => replacePrices(taxRate, currency, rates), 750);
+                    });
+                });
+            }
+
+            function watchPriceElement(selector, taxRate, currency, rates) {
+                let targetNode = document.querySelector(selector);
+                if (targetNode) {
+                    const observer = new MutationObserver(() => {
+                        observer.disconnect();
+                        replacePrices();
+                        watchPriceElement(selector, taxRate, currency, rates);
+                    });
+                    observer.observe(targetNode, { childList: true });
+                } else {
+                    setTimeout(() => watchPriceElement(selector, taxRate, currency, rates), 500);
+                }
+            }
+
+            replacePrices();
+        },
+        args: [taxRate, currency, rates, shippingCost]
     });
 }
--- options.js
+++ options.js
@@ -1,16 +1,24 @@
-// options.js
 document.getElementById('saveButton').onclick = function() {
-    let taxRate = document.getElementById('taxRate').value;
-    let currency = document.getElementById('currency').value;
-    chrome.storage.sync.set({taxRate: taxRate, currency: currency}, function() {
-      console.log('Options saved.');
-    });
-  };
-  
-  window.onload = function() {
-    chrome.storage.sync.get(['taxRate', 'currency'], function(data) {
-      document.getElementById('taxRate').value = data.taxRate || '';
-      document.getElementById('currency').value = data.currency || 'USD';
-    });
-  };
-  
+  let taxRate = document.getElementById('taxRate').value;
+  let shippingCost = document.getElementById('shipping-cost').value;
+  let currency = document.getElementById('currency').value;
+
+  chrome.storage.sync.set({taxRate: taxRate, shippingCost: shippingCost, currency: currency}, function() {
+    // Display a success message
+    let status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    
+    // Clear the message after a few seconds
+    setTimeout(function() {
+      status.textContent = '';
+    }, 3000);
+  });
+};
+
+window.onload = function() {
+  chrome.storage.sync.get(['taxRate', 'shippingCost', 'currency'], function(data) {
+    document.getElementById('taxRate').value = data.taxRate || '';
+    document.getElementById('shipping-cost').value = data.shippingCost || '19.99';
+    document.getElementById('currency').value = data.currency || 'USD';
+  });
+};
